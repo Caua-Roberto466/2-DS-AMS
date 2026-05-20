7ced32d10a6234a49a30353495494818df737c7c
Cauã Roberto <cauaroberto211@gmail.com>
2026-05-20 14:40:15 -0300

    Calculadora feita com formulário de interface

---
+++ DS/2º Bimestre/CalculadoraInterface/src/calculadorainterface/CalculadoraInterface.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadorainterface;
+import javax.swing.*;
+/**
+ *
+ * @author Admin
+ */
+public class CalculadoraInterface {
+
+    public static void main(String[] args) {
+        Interface app = new Interface();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
